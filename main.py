3813fad77f88772c0ae23e65228fa8e27327ff01
Attila Kis-Ivan <attilakis-ivan@Attilas-iMac.local>
2024-02-04 08:27:31 +0000

    fixed soem bugs

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def home():
 
 @app.route("/api/v1/<station>/<date>")
 def about(station, date):
-    filename = "/Users/attilakis-ivan/Desktop/weather-api/data_small/TG_STAID " + str(station).zfill(6) + ".txt"
+    filename = "/Users/attilakis-ivan/Desktop/weather-api/data_small/TG_STAID" + str(station).zfill(6) + ".txt"
     df =pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
     temperature = df.loc[df["    DATE"]== date]['   TG'].squeeze() / 10
     return {
@@ -24,14 +24,14 @@ def about(station, date):
     
 @app.route("/api/v1/<station>")
 def all_data(station):
-        filename = "/Users/attilakis-ivan/Desktop/Weather Datas/data_small/TG_STAID" + str(station).zfill(6) + ".txt"
+        filename = "/Users/attilakis-ivan/Desktop/weather-api/data_small/TG_STAID" + str(station).zfill(6) + ".txt"
         df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
         result = df.to_dict(orient="records")
         return result
 
 @app.route("/api/v1/yearly/<station>/<year>")
 def yearly(station, year):
-    filename = "/Users/attilakis-ivan/Desktop/Weather Datas/data_small/TG_STAID" + str(station).zfill(6) + ".txt"
+    filename = "/Users/attilakis-ivan/Desktop/weather-api/data_small/TG_STAID" + str(station).zfill(6) + ".txt"
     df = pd.read_csv(filename, skiprows=20)
     df["    DATE"] = df["    DATE"].astype(str)
     result = df[df["    DATE"].str.startswith(str(year))].to_dict(orient="records")
